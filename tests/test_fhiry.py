02d62505c5c5f6db835b54c62d4b0dd1bbf9eab2
Bell Eapen <github@gulfdoctor.net>
2020-12-14 10:27:22 -0500

    Add test files

--- tests/test_fhiry.py
+++ tests/test_fhiry.py
@@ -1,8 +1,12 @@
 import pytest
+from pkg_resources import resource_filename
 
-
+# if pkg_resources.resource_isdir(__name__, 'guest'):
+#     return pkg_resources.resource_filename(__name__, 'guest')
 
 # instance of Fhiry as fixture
+
+
 @pytest.fixture
 def f():
     from fhiry import Fhiry
@@ -11,7 +15,7 @@ def f():
 
 
 def test_set_file(f, capsys):
-    f.filename = '/gpfs/fs0/scratch/a/archer/beapen/home/scratch/fhiry/data/fhir/Aaafhir.json'
+    f.filename = resource_filename('fhiry.resources.fhir', 'afhir.json')
     print(f.get_info())
     captured = capsys.readouterr()
     assert 'memory usage' in captured.out
