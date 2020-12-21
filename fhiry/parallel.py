3364664b383a0580f862339b4098fa198e231a56
Bell Eapen <github@gulfdoctor.net>
2020-12-21 07:53:36 -0500

    Merge commit 'c9109c0e293c4c64c30d0a48d9aa491ed822f2bb'

---
+++ fhiry/parallel.py
@@ -0,0 +1,23 @@
+from fhiry import Fhiry
+import os
+import multiprocessing as mp
+import pandas as pd
+
+def process_files(file):
+    f = Fhiry()
+    return f.process_file(file)
+
+def process(folder):
+    # TODO: Fix the below error when ? folder has few files
+    # Currently falls back when it fails
+    # json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)
+    try:
+        pool = mp.Pool(mp.cpu_count())
+        list_of_dataframes = pool.map(process_files, [folder + '/' + row for row in os.listdir(folder)])
+        pool.close()
+        return pd.concat(list_of_dataframes)
+    except:
+        f = Fhiry()
+        f.folder = folder
+        f.process_df()
+        return f.df
