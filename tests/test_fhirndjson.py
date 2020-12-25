73c900459eb0ee8d5255c479ad04ff95ec231c95
Bell Eapen <github@gulfdoctor.net>
2020-12-25 12:30:47 -0500

    in progress

--- tests/test_fhirndjson.py
+++ tests/test_fhirndjson.py
@@ -12,8 +12,9 @@ def f():
 def test_process_folder(f, capsys):
     f.folder = resource_filename(__name__, 'resources')
     f.process_df()
-    print(f.df.columns)
+    # print(f.df.columns)
+    print(f.df['subject.reference'].head(5))
     # print(f.df.head(5))
-    print(f.df.info())  # 1194
-    # captured = capsys.readouterr()
+    # print(f.df.info())  # 1194
+    # captured = capsys.readouterr()s
     # assert '1194' in captured.out
