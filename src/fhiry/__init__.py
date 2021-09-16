05d329e0a734e21ec3dd1b65d0347a05f48ad8fe
Bell Eapen <github_public@gulfdoctor.net>
2021-09-16 11:21:45 -0400

    move core to src

--- src/fhiry/__init__.py
+++ src/fhiry/__init__.py
@@ -1,5 +1,8 @@
 import sys
 
+from .fhiry import Fhiry
+from .fhirndjson import Fhirndjson
+
 if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
     from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
