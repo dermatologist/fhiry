5d3b511abed15090931a1708b61b45b015a6fad1
Bell Eapen <github_public@gulfdoctor.net>
2023-03-06 18:23:20 +0000

    base class in progress

--- src/fhiry/base_fhiry.py
+++ src/fhiry/base_fhiry.py
@@ -5,17 +5,12 @@
  https://opensource.org/licenses/MIT
 """
 
-
 import pandas as pd
-import json
-import os
-from .base_fhiry import BaseFhiry
 
-class Fhiry(BaseFhiry):
+
+class BaseFhiry(object):
     def __init__(self):
         self._df = None
-        self._filename = ""
-        self._folder = ""
 
         # Codes from the FHIR datatype "coding"
         # (f.e. element resource.code.coding or element resource.clinicalStatus.coding)
@@ -35,35 +30,12 @@ class Fhiry(BaseFhiry):
         return self._df
 
     @property
-    def filename(self):
-        return self._filename
-
-    @property
-    def folder(self):
-        return self._folder
-
-    @property
     def delete_col_raw_coding(self):
         return self._delete_col_raw_coding
-
-    @filename.setter
-    def filename(self, filename):
-        self._filename = filename
-        self._df = self.read_bundle_from_file(filename)
-
-    @folder.setter
-    def folder(self, folder):
-        self._folder = folder
 
     @delete_col_raw_coding.setter
     def delete_col_raw_coding(self, delete_col_raw_coding):
         self._delete_col_raw_coding = delete_col_raw_coding
-
-    def read_bundle_from_file(self, filename):
-        with open(filename, 'r') as f:
-            json_in = f.read()
-            json_in = json.loads(json_in)
-            return pd.json_normalize(json_in['entry'])
 
     def read_bundle_from_bundle_dict(self, bundle_dict):
         return pd.json_normalize(bundle_dict['entry'])
@@ -73,35 +45,10 @@ class Fhiry(BaseFhiry):
             del self._df['resource.text.div']
 
     def process_df(self):
-        """Read a single JSON resource or a directory full of JSON resources
-        ONLY COMMON FIELDS IN ALL resources will be mapped
-        """
-        if self._folder:
-            df = pd.DataFrame(columns=[])
-            for file in os.listdir(self._folder):
-                if file.endswith(".json"):
-                    self._df = self.read_bundle_from_file(
-                        os.path.join(self._folder, file))
-                    self.delete_unwanted_cols()
-                    self.convert_object_to_list()
-                    self.add_patient_id()
-                    if df.empty:
-                        df = self._df
-                    else:
-                        df = pd.concat([df, self._df])
-            self._df = df
-        elif self._filename:
-            self._df = self.read_bundle_from_file(self._filename)
-            self.delete_unwanted_cols()
-            self.convert_object_to_list()
-            self.add_patient_id()
-
-    def process_file(self, filename):
-        self._df = self.read_bundle_from_file(filename)
         self.delete_unwanted_cols()
         self.convert_object_to_list()
         self.add_patient_id()
-        return self._df
+
 
     def process_bundle_dict(self, bundle_dict):
         self._df = self.read_bundle_from_bundle_dict(bundle_dict)
@@ -110,4 +57,55 @@ class Fhiry(BaseFhiry):
         self.add_patient_id()
         return self._df
 
+    def convert_object_to_list(self):
+        """Convert object to a list of codes
+        """
+        for col in self._df.columns:
+            if 'coding' in col:
+                codes = self._df.apply(
+                    lambda x: self.process_list(x[col]), axis=1)
+                self._df = pd.concat(
+                    [self._df, codes.to_frame(name=col+'codes')], axis=1)
+                if self._delete_col_raw_coding:
+                    del self._df[col]
+            if 'display' in col:
+                codes = self._df.apply(
+                    lambda x: self.process_list(x[col]), axis=1)
+                self._df = pd.concat(
+                    [self._df, codes.to_frame(name=col+'display')], axis=1)
+                del self._df[col]
 
+    def add_patient_id(self):
+        """Create a patientId column with the resource.id if a Patient resource or with the resource.subject.reference if other resource type
+        """
+        self._df['patientId'] = self._df.apply(lambda x: x['resource.id'] if x['resource.resourceType']
+                                               == 'Patient' else self.check_subject_reference(x), axis=1)
+
+    def check_subject_reference(self, row):
+        try:
+            return row['resource.subject.reference'].replace('Patient/', '')
+        except:
+            return ""
+
+    def get_info(self):
+        if self._df is None:
+            return "Dataframe is empty"
+        return self._df.info()
+
+    def process_list(self, myList):
+        """Extracts the codes from a list of objects
+
+        Args:
+            myList (list): A list of objects
+
+        Returns:
+            list: A list of codes
+        """
+        myCodes = []
+        if isinstance(myList, list):
+            for entry in myList:
+                if 'code' in entry:
+                    myCodes.append(entry['code'])
+                elif 'display' in entry:
+                    myCodes.append(entry['display'])
+        return myCodes
