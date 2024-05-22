e1945a77b37ea3f1757f2c9fb1061dc06816ea1f
Bell Eapen <github@gulfdoctor.net>
2024-05-22 11:24:36 -0400

    feat: Add flatten_observation method to FlattenFhir class

--- src/fhiry/flattenfhir.py
+++ src/fhiry/flattenfhir.py
@@ -36,15 +36,34 @@ class FlattenFhir(ABC):
             self._flattened = "Bundle"
         elif isinstance(self._fhirobject, Patient):
             self._flattened = self.flatten_patient(self._fhirobject)
+        elif isinstance(self._fhirobject, Observation):
+            self._flattened = self.flatten_observation(self._fhirobject)
 
     def get_timeago(self, datestring: datetime) -> str:
-        return timeago.format(datestring, datetime.datetime.now())
+        """
+        Returns a string representing the time elapsed since the given date.
+
+        :param datestring: The date to calculate the time ago from.
+        :type datestring: datetime
+        :return: A string representing the time elapsed since the given date.
+        :rtype: str
+        """
+        return timeago.format(datestring.replace(tzinfo=None), datetime.datetime.now())
 
     def flatten_patient(self, patient: Patient) -> str:
+        """
+        Flatten the patient object into a string representation.
+
+        Args:
+            patient (Patient): The patient object to be flattened.
+
+        Returns:
+            str: The flattened string representation of the patient object.
+        """
         flat_patient = ""
-        try:
+        if patient.gender:
             flat_patient += f"Medical record of a {patient.gender} patient "
-        except:
+        else:
             _logger.info(f"Gender not found for patient {patient.id}")
             flat_patient += "Medical record of a patient "
         try:
@@ -53,3 +72,49 @@ class FlattenFhir(ABC):
             _logger.info(f"Birthdate not found for patient {patient.id}")
             flat_patient += "of unknown age. "
         return flat_patient
+
+    def flatten_observation(self, observation: Observation) -> str:
+        """
+        Flatten the observation object into a string representation.
+
+        Args:
+            observation (Observation): The observation object to be flattened.
+
+        Returns:
+            str: The flattened string representation of the observation object.
+        """
+        flat_observation = ""
+        if observation.code:
+            flat_observation += f"{observation.code.coding[0].display} "
+        else:
+            _logger.info(f"Code not found for observation {observation.id}")
+            flat_observation += "Observation "
+        if observation.effectiveDateTime:
+            flat_observation += f"recorded {self.get_timeago(observation.effectiveDateTime)} was "
+        else:
+            _logger.info(f"Effective date not found for observation {observation.id}")
+            flat_observation += "of unknown date was "
+        if observation.valueQuantity:
+            flat_observation += f"Value: {observation.valueQuantity.value} {observation.valueQuantity.unit}. "
+        elif observation.valueString:
+            flat_observation += f"Value: {observation.valueString}. "
+        elif observation.valueBoolean:
+            flat_observation += f"Value: {observation.valueBoolean}. "
+        elif observation.valueRange:
+            flat_observation += f"Value: {observation.valueRange.low.value} - {observation.valueRange.high.value} {observation.valueRange.low.unit}. "
+        elif observation.valueRatio:
+            flat_observation += f"Value: {observation.valueRatio.numerator.value} {observation.valueRatio.numerator.unit} / {observation.valueRatio.denominator.value} {observation.valueRatio.denominator.unit}. "
+        elif observation.valuePeriod:
+            flat_observation += f"Value: {observation.valuePeriod.start} - {observation.valuePeriod.end}. "
+        elif observation.valueDateTime:
+            flat_observation += f"Value: {observation.valueDateTime}. "
+        elif observation.valueTime:
+            flat_observation += f"Value: {observation.valueTime}. "
+        elif observation.valueSampledData:
+            flat_observation += f"Value: {observation.valueSampledData.data}. "
+        else:
+            _logger.info(f"Value not found for observation {observation.id}")
+            flat_observation += "Value: unknown. "
+        if observation.interpretation[0].text:
+            flat_observation += f"Interpretation: {observation.interpretation[0].text}. "
+        return flat_observation
--- tests/test_flattenfhir.py
+++ tests/test_flattenfhir.py
@@ -1,6 +1,7 @@
 import pytest
 from fhir.resources.bundle import Bundle
 from fhir.resources.patient import Patient
+from fhir.resources.observation import Observation
 from src.fhiry.flattenfhir import FlattenFhir
 from pkg_resources import resource_filename
 import datetime
@@ -19,3 +20,9 @@ def test_flatten_patient():
     flatten_fhir = FlattenFhir(patient)
     assert flatten_fhir.flattened == "Medical record of a male patient born 49 years ago. "
 
+def test_flatten_observation():
+    # Test with Observation
+    jsonfile = open(resource_filename(__name__, 'resources') + '/flattenfhir/observation.json')
+    observation = Observation.parse_raw(jsonfile.read())
+    flatten_fhir = FlattenFhir(observation)
+    assert flatten_fhir.flattened == "RBS recorded 11 years ago was Value: 6.3 mmol/l. "
