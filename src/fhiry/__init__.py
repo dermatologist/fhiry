cc60d58833344f7b45abe747b6f1f97a84343287
dermatologist <github@gulfdoctor.net>
2025-11-10 06:44:36 -0600

    fix: comment out unused BQsearch import in __init__.py

--- src/fhiry/__init__.py
+++ src/fhiry/__init__.py
@@ -3,7 +3,7 @@ import sys
 from .fhiry import Fhiry
 from .fhirndjson import Fhirndjson
 from .fhirsearch import Fhirsearch
-from .bqsearch import BQsearch
+# from .bqsearch import BQsearch
 from .flattenfhir import FlattenFhir
 
 if sys.version_info[:2] >= (3, 8):
